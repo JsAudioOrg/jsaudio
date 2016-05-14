38c15a3cfbde20902da893e84a9667c4cd7b2395
Andrew Carpenter <andrew@audioinhd.com>
2016-05-14 14:45:46 -0400

    Start on tests using ava

--- test/test.js
+++ test/test.js
@@ -6,17 +6,42 @@ const JsAudio = JsAudioExports.JsAudioNative
 // const JsPaStream = JsAudio.JsPaStream
 
 test('JsAudio.initialize', (t) => {
-  return t.truthy(JsAudio.initialize(), 'successfully initialized')
+  return t.truthy(JsAudio.initialize())
 })
-test.todo('JsAudio.terminate')
-test.todo('JsAudio.getVersion')
-test.todo('JsAudio.getHostApiCount')
-test.todo('JsAudio.getDefaultHostApi')
-test.todo('JsAudio.getHostApiInfo')
-test.todo('JsAudio.getDeviceCount')
-test.todo('JsAudio.getDefaultInputDevice')
-test.todo('JsAudio.getDefaultOutputDevice')
-test.todo('JsAudio.getDeviceInfo')
+test('JsAudio.getVersion', (t) => {
+  return t.truthy(Number.isInteger(JsAudio.getVersion()))
+})
+test('JsAudio.getHostApiCount', (t) => {
+  return t.truthy(Number.isInteger(JsAudio.getHostApiCount()))
+})
+test('JsAudio.getDefaultHostApi', (t) => {
+  return t.truthy(Number.isInteger(JsAudio.getDefaultHostApi()))
+})
+test('JsAudio.getHostApiInfo', (t) => {
+  let hostApiInfo = JsAudio.getHostApiInfo(JsAudio.getDefaultHostApi())
+  return t.truthy(
+    hostApiInfo &&
+    hostApiInfo.hasOwnProperty('apiIndex') &&
+    hostApiInfo.apiIndex >= 0
+  )
+})
+test('JsAudio.getDeviceCount', (t) => {
+  return t.truthy(Number.isInteger(JsAudio.getDeviceCount()))
+})
+test('JsAudio.getDefaultInputDevice', (t) => {
+  return t.truthy(Number.isInteger(JsAudio.getDefaultInputDevice()))
+})
+test('JsAudio.getDefaultOutputDevice', (t) => {
+  return t.truthy(Number.isInteger(JsAudio.getDefaultOutputDevice()))
+})
+test('JsAudio.getDeviceInfo', (t) => {
+  let dvcInfo = JsAudio.getDeviceInfo(JsAudio.getDefaultInputDevice())
+  return t.truthy(
+    dvcInfo &&
+    dvcInfo.hasOwnProperty('deviceIndex') &&
+    dvcInfo.deviceIndex >= 0
+  )
+})
 test.todo('JsAudio.openStream')
 test.todo('JsAudio.openDefaultStream')
 test.todo('JsAudio.startStream')
@@ -24,3 +49,6 @@ test.todo('JsAudio.getStreamWriteAvailable')
 test.todo('JsAudio.getStreamReadAvailable')
 test.todo('JsAudio.writeStream')
 test.todo('JsAudio.readStream')
+test('JsAudio.terminate', (t) => {
+  return t.truthy(JsAudio.terminate())
+})
