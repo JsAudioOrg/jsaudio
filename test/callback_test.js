86a9fa8d682b1eec020d160114d5629d9cc463ae
Jacob Peters <jacobcpeters@gmail.com>
2016-06-23 04:00:44 -0400

    Add sine output to test script

--- test/callback_test.js
+++ test/callback_test.js
@@ -12,9 +12,9 @@ http://portaudio.com/docs/v19-doxydocs/paex__write__sine_8c_source.html
 // Setup
 const sampleRate = 48000
 const channels = 2
-const framesPerBuffer = 8192
+const framesPerBuffer = 2048
 const streamFlags = 0
-const tableSize = 200
+const tableSize = 400
 const formats = {
   paFloat32: 1,
   paInt32: 2,
@@ -26,6 +26,34 @@ const formats = {
   paNonInterleaved: 2147483648
 }
 
+var left_phase = 0
+var right_phase = 0
+var sine = []
+
+// initialise sinusoidal wavetable
+for(var i=0; i<tableSize; i++ )
+{
+  sine[i] = Math.sin((i/tableSize) * Math.PI * 2 )
+}
+
+function SineCallback(input, output, frameCount) {
+  var outputBufferView = new Float32Array(output);
+  
+  for(var i=0; i<frameCount*2; i+=2 )
+  {
+    outputBufferView[i] = sine[left_phase]  /* left */
+    outputBufferView[i+1] = sine[right_phase]  /* right */
+    left_phase += 1
+    if( left_phase >= tableSize ) left_phase -= tableSize;
+    right_phase += 3 /* higher pitch so we can distinguish left and right. */
+    if( right_phase >= tableSize ) right_phase -= tableSize;
+  }
+}
+
+
+
+
+//Main function
 function blockingSine () {
   // initialize stream instance
   let stream = new JsPaStream()
@@ -41,10 +69,10 @@ function blockingSine () {
     sampleFormat: formats.paFloat32,
     numInputChannels: channels,
     numOutputChannels: channels,
-    callback: ()=> {console.log('Callback')}
+    callback: SineCallback
   }
   // open stream with options
-  console.log(JsAudio.openDefaultStream(streamOpts));
+  console.log(JsAudio.openDefaultStream(streamOpts))
   
   JsAudio.startStream(stream);
 }
